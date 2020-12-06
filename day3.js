28da3601ac2f0668c36bfd5d9aa345d8dc463c4c
dukemai <duc.mai@bonniernews.se>
2020-12-06 23:03:21 +0100

    add day3

--- day3.js
+++ day3.js
@@ -1,31 +1,47 @@
 const data = require("./day3.json").filter(Boolean);
+const sample = require("./day3-sample.json").filter(Boolean);
 
 const getBreadthWidth = (map) => map[0].length;
 
 // down 1, right 3
-const travel = (map, curr, visited) => {
+const travel = (map, curr, visited, planStep) => {
+  const plannedRow = curr.row + planStep.down;
   const destination = {
-    row: curr.row + 1,
-    column: (curr.column + 3) % getBreadthWidth(map),
+    row: plannedRow >= map.length ? map.length - 1 : plannedRow,
+    column: (curr.column + planStep.right) % getBreadthWidth(map),
   };
-  if(!map[destination.row]) return null;
+  if (!map[destination.row]) return null;
 
   const next = [...map[destination.row]][destination.column];
   visited.push(next);
-  if (destination.row === map.length) {
+  if (destination.row === map.length - 1) {
     return null;
   }
   return destination;
 };
 
-const doPart1 = (input) => {
+// this time will travel by a plan
+const plan = [
+  { right: 1, down: 1 },
+  { right: 3, down: 1 },
+  { right: 5, down: 1 },
+  { right: 7, down: 1 },
+  { right: 1, down: 2 },
+];
+
+const doPart1 = (input, planStep = { right: 3, down: 1 }) => {
   let curr = { row: 0, column: 0 };
   const visited = [curr];
   while (curr !== null) {
-    curr = travel(input, curr, visited);
+    curr = travel(input, curr, visited, planStep);
   }
-  const treeCount = visited.map(v => v ==='#').filter(Boolean).length;
+  const treeCount = visited.map((v) => v === "#").filter(Boolean).length;
   return treeCount;
 };
 
-console.log(doPart1(data));
+const doPart2 = (input) => {
+  const total = plan.reduce((acc, planStep) => acc * doPart1(input, planStep), 1);
+  return total;
+};
+
+console.log(doPart2(data));
