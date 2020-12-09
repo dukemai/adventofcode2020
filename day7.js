9ec86006bb1c0c5a87163103e6fca1297d163e99
dukemai <duc.mai@bonniernews.se>
2020-12-09 22:40:53 +0100

    add day 7

--- day7.js
+++ day7.js
@@ -20,8 +20,8 @@ const doPart1 = (input, bagToFind) => {
   }, {});
 
   const directlyContainingBags = Object.entries(rules).reduce(
-    (acc, [key, value]) => { 
-      if (value.find(item => item.match(bagToFind))) {
+    (acc, [key, value]) => {
+      if (value.find((item) => item.match(bagToFind))) {
         acc.add(key);
       }
       return acc;
@@ -29,7 +29,7 @@ const doPart1 = (input, bagToFind) => {
     new Set()
   );
   const indirectlybags = [...directlyContainingBags].reduce((acc, bag) => {
-    (doPart1(input, bag.replace(/bags$/, 'bag'))).forEach((item) =>
+    doPart1(input, bag.replace(/bags$/, "bag")).forEach((item) =>
       acc.add(item)
     );
     return acc;
@@ -37,5 +37,40 @@ const doPart1 = (input, bagToFind) => {
   summary[bagToFind] = new Set([...directlyContainingBags, ...indirectlybags]);
   return summary[bagToFind];
 };
-console.log(doPart1(data, "shiny gold bag").size);
-// console.log(summary);
+
+const getNumberOfBags = (bags) =>
+  /^\d+/.test(bags) ? +/^\d+/.exec(bags)[0] : 0;
+
+const whatCanBeInTheBagPart2 = (input, bagToFind) => {
+  const rules = input.reduce((acc, curr) => {
+    const { containingBags, containedBags } = parseSentence(curr);
+    acc[containingBags] = containedBags;
+    return acc;
+  }, {});
+
+  const directlyContainedBags = rules[`${bagToFind}s`];
+  if (directlyContainedBags) {
+    const total = [...directlyContainedBags].reduce((acc, bag) => {
+      return (
+        acc +
+        getNumberOfBags(bag) +
+        getNumberOfBags(bag) *
+          (whatCanBeInTheBagPart2(
+            input,
+            bag.replace(/\d\s/, "").replace(/bags$/, "bag")
+          ))
+      );
+    }, 0);
+    summary[bagToFind] = total;
+    return summary[bagToFind];
+  }
+  return 0;
+};
+
+const doPart2 = (input, bagToFind) => {
+  const result = whatCanBeInTheBagPart2(input, bagToFind);
+
+  return result;
+};
+
+console.log(doPart2(data, "shiny gold bag"));
